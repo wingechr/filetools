b15dbc0d79c867d97717e9250f3e0fc78d31eea3
Christian Winger <c@wingechr.de>
2019-12-09 14:28:24 +0100

    initial commit

---
+++ filetools/setup.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="filetools",
+    version="0.0.1",
+    author="Christian Winger",
+    author_email="c@wingechr.de",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/wingechr/filetools",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
