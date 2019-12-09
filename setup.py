6fab6463d5f12c055bc756680059442b5e68678f
Christian Winger <c@wingechr.de>
2019-12-09 16:13:05 +0100

    tests

--- setup.py
+++ setup.py
@@ -6,13 +6,17 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="filetools",
-    version="0.0.3",
+    version="0.0.4",
     author="Christian Winger",
     author_email="c@wingechr.de",
     description="",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/wingechr/filetools",
+    url="",
+    project_urls={
+        'Documentation': 'https://filetools.readthedocs.io',
+        'Source': 'https://github.com/wingechr/filetools',        
+    },
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
